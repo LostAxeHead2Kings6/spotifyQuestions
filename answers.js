70e198aa782307ca23a7e8619822c2d56307ac7d
Admin <admin@Admins-MacBook-Air.local>
2018-06-13 16:05:52 -0400

    second

--- answers.js
+++ answers.js
@@ -3,5 +3,18 @@ function sortByStrings(s, t) {
 }
 
 function decodeString(s) {
-  
+  let output = [];
+  const integers = ['1', '2', '3', '4', '5',
+                    '6', '7', '8', '9'];
+
+  function recursion(s) {
+   //remove layer of brackets.
+      //remainder = split[1].slice(0,-1);
+
+   //separate integers from characters  in split[0]
+
+  //return integer * recursion(remainder)
+  }
+
+  return recursion(s);
 }
